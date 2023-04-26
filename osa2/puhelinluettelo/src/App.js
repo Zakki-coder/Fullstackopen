ee8030ad11be3816ca5ca90048bef6e4f4c33f23
jniemine <nieminen.jakke@gmail.com>
2023-04-26 19:42:32 +0300

    puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-	{ name: 'Arto Hellas' }
+	{ name: 'Arto Hellas', number: "040-1231244" }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNumber] = useState('')
   let id = 0
 
   const handleNameChange = (event) => {
@@ -13,15 +14,23 @@ const App = () => {
 
   const addContact = (event) => {
 	event.preventDefault()
+
 	if (!persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
-		setPersons(persons.concat({name: newName}))
+		if (newName.length > 0)
+			setPersons(persons.concat({name: newName, number:newNumber}))
+		else
+			alert(`You have given an empty name`)
 	}
 	else 
 		alert(`${newName} is already added to phonebook`)
   }
 
   const Person = ({person}) => {
-	return <li>{person.name}</li>
+	return <li>{person.name} {person.number}</li>
+  }
+
+  const handleNumber = (event) => {
+	setNumber(event.target.value)
   }
 
   return (
@@ -32,6 +41,11 @@ const App = () => {
 		  name: <input 
 		   value={newName}
 		   onChange={handleNameChange}/>
+		</div>
+		<div>
+			number: <input
+				value={newNumber}
+				onChange={handleNumber}/>
 		</div>
 		<div>
 		  <button type="submit">add</button>
